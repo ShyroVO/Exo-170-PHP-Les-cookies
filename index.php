cb5baf0b1d9a1a46bb10b58b961d4662bbcc9065
ShyroVO <Bosquet.laurie@outlook.com>
2021-02-03 14:05:48 +0100

    1.0 / Cookie valide fonctionel

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
-
+/*
+setcookie('theme', 'light');
+echo $_COOKIE['theme']; */
 
 /**
  * 3. Commentez le code du point 1 et 2
- * 4. Créez un cookie de nom username ayant pour valeur votre nom et expirant dans 2 jours et 2h, le domaine doit être localhost ( votre machine )
+ * 4. Créez un cookie de nom username ayant pour valeur votre nom et expirant dans 2 jours et 2h,
+ * le domaine doit être localhost ( votre machine )
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+setcookie('username', 'myName', time()+((86400 * 2) + (3600 * 2)), '/', 'localhost');
